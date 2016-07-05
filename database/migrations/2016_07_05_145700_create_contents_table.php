f7a7171cfebe227ee6e48a26959ab2589915f907
Sylvian ROPERT <sylvian.ropert@gmail.com>
2016-07-05 17:20:57 +0200

    Creating Migrations and setting up pgsql SGBD
    Needs to have composer insall done right after pull, and then php artisan migrate then php artisan make:models

---
+++ database/migrations/2016_07_05_145700_create_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateContentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('contents', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('title', 45)->nullable();
+			$table->text('content', 65535)->nullable();
+			$table->string('url', 45)->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('contents');
+	}
+
+}
---
+++ database/migrations/2016_07_05_145700_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->integer('vat')->nullable();
+			$table->string('status', 12)->nullable();
+			$table->integer('user_id',false,true);
+			$table->date('created_at')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
