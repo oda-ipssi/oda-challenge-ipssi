9e534ee165f2497375626db3026fd7229bf83436
winceweb <winceweb@gmail.com>
2016-07-07 14:42:33 +0200

    Seeders Users, Roles, Permissions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call('UsersTableSeeder');
+        $this->call('RolesTableSeeder');
+        $this->call('PermissionsTableSeeder');
     }
 }
