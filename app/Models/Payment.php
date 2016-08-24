15c05f9dda3110138066767099b5a9e30529146e
fey89 <feyza.kozan04@gmail.com>
2016-08-24 15:12:58 +0200

    Payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+*	Class Payment
+*/
+class Payment extends Model
+{
+    protected $table = 'payments';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'order_id',
+        'billingType',
+        'amount',
+        'cardNumber',
+        'expirationDate',
+        'created_at'
+    ];
+
+    protected $guarded = [];
+}
